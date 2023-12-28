477087f3e8c0ab2685e670198ae0144cf7c2a357
ElaYJ <syunjae21@gmail.com>
2023-12-29 02:24:04 +0900

    math modify_2

--- Part2_Math/11_FibonacciSequence.py
+++ Part2_Math/11_FibonacciSequence.py
@@ -53,14 +53,32 @@ print('{}번째 항의 값: {}'.format(input_num, value_an))
 print('{}번째 항까지의 합: {}'.format(input_num, sum_an))
 print()
 
+
+# 1, 1, 2, 3, 5, 8, 13, 21, ....
 def fibonacciRecursion(n):
     if n <= 1: return n
     else:
         return fibonacciRecursion(n-2) + fibonacciRecursion(n-1)
 
-print('피보나치 수: { ', end='')
-for n in range(1, inputN):
+print('피보나치 수: ( ', end='')
+for n in range(1, inputN + 1):
     print(fibonacciRecursion(n), end=', ')
-print(fibonacciRecursion(inputN), '}')
+print('... )')
 
 print('{}번째 항의 값: {}'.format(inputN, fibonacciRecursion(inputN)))
+print()
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+def fibo_recur(n):
+    if n == 1: return 0
+    elif n == 2: return 1
+    else:
+        return fibo_recur(n-2) + fibo_recur(n-1)
+
+print('피보나치 수: ( ', end='')
+for n in range(1, inputN + 1):
+    print(fibo_recur(n), end=', ')
+print('... )')
+
+print('{}번째 항의 값: {}'.format(inputN, fibo_recur(inputN)))
+print()
