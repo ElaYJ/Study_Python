2d8b6fb4c049cc5a1462bc1def553c36c691487d
ElaYJ <syunjae21@gmail.com>
2024-09-04 23:03:21 +0900

    2024/09/04

--- test.py
+++ test.py
@@ -2,4 +2,15 @@
 print(a, type(a), b, type(b))
 
 
+class Family:
+    lastname = '김'
+
+print(Family.lastname)
+
+a = Family()
+b = Family()
+print(a.lastname, b.lastname)
+
+a.lastname = "최"
+print(a.lastname, b.lastname, Family.lastname)
 
