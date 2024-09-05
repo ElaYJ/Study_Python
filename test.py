e8ddb6e1a45151b5fa52d216fdc8b2e3fc14c998
ElaYJ <syunjae21@gmail.com>
2024-09-05 18:01:09 +0900

    2024/09/05

--- test.py
+++ test.py
@@ -14,3 +14,15 @@ print(a.lastname, b.lastname)
 a.lastname = "최"
 print(a.lastname, b.lastname, Family.lastname)
 
+
+a = [1, 2, 3]
+b = list(a)
+a[1] = 0
+print(a, b)
+
+students = ['한민서', '황지민', '이영철', '이광수', '김승민']
+snacks = ['사탕', '초컬릿', '젤리']
+
+result = zip(students, snacks)
+print(list(result))
+
